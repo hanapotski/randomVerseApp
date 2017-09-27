3be4b9ed7cfae3a5fb14393594e38228bf03fd73
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-27 11:00:10 -0700

    add button functionality

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 $(function() {
   // Handler for .ready() called.
-  
+  $("#newButton").on("click", function(){
+  // Click event handler
+    $.getJSON("bible-api.com/john 3:16", function(json){
+      $("#proverbContainer").html(JSON.stringify(json));
+    });
+  });
+});
+
+
+function getNewProverb(){
+  $.getJSON("bible-api.com/john 3:16", function(json){
+    $("#proverbContainer").html(JSON.stringify(json));
+  });
+}
+
+
+app.listen(3000, function(){
+  console.log("SERVER IS RUNNING!");
 });
