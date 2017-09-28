43b1cca490f5903abe0715562d38be7f97ede21f
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-28 15:10:40 -0700

    add verses

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-$(function() {
-  // Handler for .ready() called.
-  $("#newButton").on("click", function(){
-  // Click event handler
-    $.getJSON("bible-api.com/john 3:16", function(json){
-      $("#proverbContainer").html(JSON.stringify(json));
-    });
-  });
-});
+var verses = [
+  {
+    text: "Watch over your heart with all diligence,\nFor from it flow the springs of life.",
+    findIn: "Proverbs 4:23"
+  },
+  {
+    text: "Iron sharpens iron,\nSo one man sharpens another.",
+    findIn: "Proverbs 27:17"
+  },
+  {
+    text: "A joyful heart is good medicine,\nBut a broken spirit dries up the bones.",
+    findIn: "Proverbs 17:22"
+  },
+  {
+    text: "A gentle answer turns away wrath,\nBut a harsh word stirs up anger.",
+    findIn: "Proverbs 15:1"
+  },
+  {
+    text: "Death and life are in the [a]power of the tongue,\nAnd those who love it will eat its fruit.",
+    findIn: "Proverbs 18:21"
+  },
 
-
-function getNewProverb(){
-  $.getJSON("bible-api.com/john 3:16", function(json){
-    $("#proverbContainer").html(JSON.stringify(json));
-  });
-}
-
-
-app.listen(3000, function(){
-  console.log("SERVER IS RUNNING!");
-});
+];
