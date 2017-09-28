fc48a7d6f1c5abd0bea24cb068136d0bea56477d
hanapotski <hannahjoy.zulueta@gmail.com>
2017-09-28 16:07:59 -0700

    fixed counter

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var verse = 0;
+
 var verses = [
   {
     text: "Watch over your heart with all diligence,For from it flow the springs of life.",
@@ -19,29 +21,48 @@ var verses = [
     text: "Death and life are in the power of the tongue,And those who love it will eat its fruit.",
     findIn: "Proverbs 18:21"
   },
+  {
+    text: "He who despises his neighbor lacks sense,But a man of understanding keeps silent.",
+    findIn: "Proverbs 11:12"
+  },
+  {
+    text: "A good name is to be more desired than great wealth,Favor is better than silver and gold.",
+    findIn: "Proverbs 22:1"
+  },
+  {
+    text: "He who walks with wise men will be wise,But the companion of fools will suffer harm.",
+    findIn: "Proverbs 13:20"
+  },
+  {
+    text: "The mouth of the righteous is a fountain of life,But the mouth of the wicked conceals violence.",
+    findIn: "Proverbs 10:11"
+  },
+  {
+    text: "He who digs a pit will fall into it,And he who rolls a stone, it will come back on him.",
+    findIn: "Proverbs 26:27"
+  },
 ];
 
-var verse = 0;
+$(document).ready(function(){
+// start with a new verse
+newVerse();
 
-// var i = 0;
-// while (var i <= 5){
-//
-// }
 $("#button").on("click", function(){
-  if (verse === 5){
+  if (verse === 9){
     reset();
     newVerse();
-  } else{
+  } else {
     newVerse();
-    verse++;
   }
 });
 
 function newVerse(){
   $("#text").text(verses[verse].text);
   $("#findIn").text(verses[verse].findIn);
+  verse++;
 }
 
 function reset(){
-  verse = 0;
-}
+    verse = 0;
+  }
+});
